4858ea8ada7f823f56be4c75076096e7c0c5d66f
MarcosLaydner <30905990+MarcosLaydner@users.noreply.github.com>
2018-11-21 20:00:29 -0200

    Question Generator reads and parses a json file

--- FaustopOlympics/src/br/ufsc/FasutopOlympics/utilities/QuestionGenerator.java
+++ FaustopOlympics/src/br/ufsc/FasutopOlympics/utilities/QuestionGenerator.java
@@ -1,7 +1,15 @@
 package br.ufsc.FasutopOlympics.utilities;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import br.ufsc.FasutopOlympics.model.Question;
 
@@ -28,6 +36,23 @@ public class QuestionGenerator {
 	}
 	
 	private void store() {
-		
+		JSONParser parser = new JSONParser();
+		try {
+			JSONArray questions = (JSONArray) parser.parse(new FileReader("resources\\questions.json"));
+			
+			for (Object question: questions) {
+				questionBuffer.add((Question) question);
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 	}
 }
+
+
+
